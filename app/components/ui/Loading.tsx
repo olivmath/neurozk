e89e04f2d3d395a9d554c6696441120eee7055c7
Lucas Oliveira <olivmath@protonmail.com>
2025-05-02 00:36:25 -0300

    try reset 2

--- app/components/ui/Loading.tsx
+++
@@ -1,7 +0,0 @@
-export const Loading = () => {
-  return (
-    <div className="bg-gray-200 text-gray-700 font-bold py-2 px-4 rounded">
-      Loading...
-    </div>
-  );
-}; 
--- app/utils/formatAddress.ts
+++
@@ -1,6 +0,0 @@
-import { Address } from "@ton/core";
-
-export const formatAddress = (address: string): string => {
-  const tempAddress = Address.parse(address).toString();
-  return `${tempAddress.slice(0, 4)}...${tempAddress.slice(-4)}`;
-}; 
